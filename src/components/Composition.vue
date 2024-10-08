9cf0520f87377a440f5b118175cb0fcf1ef1c708
venugopal5501 <venugopal14082002@gmail.com>
2024-10-08 11:47:13 +0530

    Composition

---
+++ src/components/Composition.vue
@@ -0,0 +1,38 @@
+<template>
+</template>
+
+<script>
+import { ref, computed, watch } from 'vue'
+export default {
+    name: 'CompositionPage',
+    setup() {
+        const newItem = ref('')
+        const items = ref([])
+        const itemsCount = computed(() => { return items.value.length })
+        function addItem() {
+            let item = { name: newItem.value }
+            items.value.push(item)
+            newItem.value = ''
+        }
+        function deleteItem(index) {
+            items.value.splice(index, 1)
+        }
+        watch(newItem, (newValue) => {
+            if (itemNotAvailable.includes(newValue.toLowerCase())) {
+                newItem.value = ''
+                alert('You never get this ' + newValue + ' item')
+            }
+        })
+        return {
+            newItem,
+            items,
+            addItem,
+            deleteItem,
+            itemsCount
+        }
+
+    }
+}
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import OptionPage from '@/components/Option.vue'
+import CompositionPage from '@/components/Composition.vue'
 
 const routes = [
   {
@@ -8,12 +9,9 @@ const routes = [
     component: OptionPage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path:'/composition',
+    name: 'CompositionPage',
+    component: CompositionPage
   }
 ]
 
